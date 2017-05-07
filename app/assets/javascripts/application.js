aa7474f774a19a45597386ceda202d82c19c3eb0
Ryan Caplin <ryan.caplin@gmail.com>
2017-05-06 22:08:04 -0600

    Reorder Javascript to Fix Signout Bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require bootstrap-sprockets
+//= require_jquery
 //= require rails-ujs
 //= require turbolinks
-//= require_tree
-//= require_jquery
+//= require bootstrap-sprockets
+//= require_tree .
